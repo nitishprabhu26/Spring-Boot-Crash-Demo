38960c732a3fc085f2f4489e52f23421974df1d4
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-09-08 16:49:42 -0500

    added new package, class 'student' and then defined it

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -21,8 +24,23 @@ public class DemoApplication {
 //		return "Hello World";
 //	}
 
+//	or
+
+//	@GetMapping
+//	public List<String> hello(){
+//		return List.of("Hello" , "World");
+//	}
+
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello" , "World");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Nitish,",
+						"nitish.prabhu26@gmail.com",
+						LocalDate.of(1994, Month.OCTOBER,26),
+						26
+				)
+		);
 	}
 }
